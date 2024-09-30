aa00156e82100872bbb2ea77fd748dc57ebc7008
sungwookcho@LAPTOP <fdsa7563@gmail.com>
2024-10-01 00:10:02 +0900

    토대 생성

--- CG_00/0930_test06.cpp
+++ CG_00/0930_test06.cpp
@@ -4,3 +4,115 @@
 #include <gl/freeglut_ext.h>
 #include <random>
 
+// 클라이언트 크기
+#define clientWidth 800
+#define clientHeight 600
+
+// 클라이언트 배경색
+GLclampf clientRed = 1.0f;
+GLclampf clientGreen = 1.0f;
+GLclampf clientBlue = 1.0f;
+
+// 랜덤 실수값(min ~ max) 반환 함수
+std::random_device rd;
+std::mt19937 gen(rd()); // Mersenne Twister 엔진
+float generateRandomFloat(float min, float max)
+{
+	std::uniform_real_distribution<float> dis(min, max);	// 인자로 범위 설정
+	return dis(gen);
+}
+
+//좌표 변환 함수
+int GL_to_Win_X(float x)
+{
+	return (x + 1) * (clientWidth / 2.0f);  // 2.0f로 실수 나눗셈
+}
+int GL_to_Win_Y(float y)
+{
+	return (1 - y) * (clientHeight / 2.0f);  // 2.0f로 실수 나눗셈
+}
+float Win_to_GL_X(int x)
+{
+	return (x / (float)clientWidth) * 2 - 1;  // 정수 나눗셈 방지
+}
+float Win_to_GL_Y(int y)
+{
+	return 1 - (y / (float)clientHeight) * 2;  // 정수 나눗셈 방지
+}
+
+
+// 화면 출력
+void draw()
+{
+	//glColor3f(r,g,b);
+	//glRectf(x1, y1, x2, y2);
+}
+
+// 왼쪽 마우스 클릭 확인
+bool left_button = false;
+
+// GL 이벤트 함수
+GLvoid drawScene(GLvoid);
+GLvoid Reshape(int w, int h);
+GLvoid Keyboard(unsigned char key, int x, int y);
+void Mouse(int button, int state, int x, int y);
+void Motion(int x, int y);
+
+void main(int argc, char** argv) //--- 윈도우 출력하고 콜백함수 설정
+{
+	//--- 윈도우 생성하기
+	glutInit(&argc, argv);								//--- glut 초기화
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);		//--- 디스플레이 모드 설정
+	glutInitWindowPosition(0, 0);						//--- 윈도우의 위치 지정
+	glutInitWindowSize(clientWidth, clientHeight);		//--- 윈도우의 크기 지정
+	glutCreateWindow("test 04");						//--- 윈도우 생성(윈도우 이름)
+
+	//--- GLEW 초기화하기
+	glewExperimental = GL_TRUE;
+	if (glewInit() != GLEW_OK) //--- glew 초기화
+	{
+		std::cerr << "Unable to initialize GLEW" << std::endl;
+		exit(EXIT_FAILURE);
+	}
+	else
+		std::cout << "GLEW Initialized\n";
+
+	//--여기서 구조체 초기화 가능--
+
+	glutDisplayFunc(drawScene);					// 출력 콜백함수의 지정
+	glutReshapeFunc(Reshape);					// 다시 그리기 콜백함수 지정
+	glutKeyboardFunc(Keyboard);					// 키보드 입력 콜백함수 지정
+	glutMouseFunc(Mouse);						// 마우스 입력
+	glutMotionFunc(Motion);						// 마우스 움직임
+	glutMainLoop();								// 이벤트 처리 시작
+}
+
+GLvoid drawScene() //--- 콜백 함수: 그리기 콜백 함수
+{
+	//--- 변경된 배경색 설정
+	glClearColor(clientRed, clientGreen, clientBlue, 1.0f);			//--- 바탕색을 변경
+	glClear(GL_COLOR_BUFFER_BIT);									//--- 설정된 색으로 전체를 칠하기
+
+	draw();		// draw -> WM_PAINT
+	glutSwapBuffers();												//--- 화면에 출력하기
+}
+GLvoid Reshape(int w, int h) //--- 콜백 함수: 다시 그리기 콜백 함수
+{
+	glViewport(0, 0, w, h);
+}
+GLvoid Keyboard(unsigned char key, int x, int y)
+{
+	switch (key)
+	{
+	case 'q':		// 프로그램 종료
+	{
+		std::cout << "Quit\n";
+		glutLeaveMainLoop(); // OpenGL 메인 루프 종료
+		break;
+	}
+	case 'r':		// 타이머 중지, 사각형 초기화 및 재입력
+		break;
+	}
+	glutPostRedisplay(); //--- refresh
+}
+
