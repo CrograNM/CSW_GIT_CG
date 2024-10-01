9474aef9526adeddd83e2a25d92d61b42b684ccd
sungwookcho@LAPTOP <fdsa7563@gmail.com>
2024-10-01 18:00:08 +0900

    사각형 분할(좌우상하) 성공

--- CG_00/0930_test06.cpp
+++ CG_00/0930_test06.cpp
@@ -44,7 +44,7 @@ float Win_to_GL_Y(int y)
 #define MIN_RECT 5
 #define MAX_RECT 10
 int create_rect_count = 0;
-typedef struct RECTANGLES
+typedef struct RECTS
 {
 	bool exist;
 	float midX;
@@ -54,6 +54,18 @@ typedef struct RECTANGLES
 	float height;
 }RECTS;
 RECTS rt[MAX_RECT];
+typedef struct DIVRECTS
+{
+	float midX;
+	float midY;
+	float r, g, b;
+	float width;
+	float height;
+	float dx, dy;
+}DIVRECTS;
+// 하나의 사각형에서 쪼개진 divRect는 최대 8개의 사각형을 저장한다.
+DIVRECTS divRect[MAX_RECT][8] = {}; // (시계방향) 0: 상, 1: 우상, 2: 우, 3: 우하, 4: 하, 5: 좌하, 6: 좌, 7: 좌상
+int div_rect_count = 0;
 
 // 사각형 생성
 void setRects()
@@ -88,7 +100,7 @@ void setRects()
 }
 
 // 사각형 나누기
-void devideRect(float mX, float mY)
+void divideRect(float mX, float mY)
 {
 	//마우스를 클릭하면 사각형 4개로 나눠서 출력
 	//해당 사각형들은 기존 사각형의 색을 유지하며, 각 사각형의 크기는 기존의 절반크기
@@ -100,17 +112,74 @@ void devideRect(float mX, float mY)
 			if (rt[i].midX - rt[i].width / 2 < mX && mX < rt[i].midX + rt[i].width / 2 &&
 				rt[i].midY - rt[i].height / 2 < mY && mY < rt[i].midY + rt[i].height / 2)
 			{
+				std::cout << "select success\n";
 				select_rect = i;		// 인덱스 저장
 				break;					// for문 탈출
 			}
 		}
 	}
-	if (select_rect >= 0)	//선택에 성공한 경우 아래 코드 실행
+	//선택에 성공한 경우 아래 코드 실행
+	if (select_rect >= 0)
 	{
-		//1. 4, 좌우상하 이동
-		//2. 4, 대각선 이동
-		//3. 4, 쪼개지고, 한쪽 방향으로 같이 이동
-		//4. 8, 8방향 이동
+		// 1. 4, 좌우상하 이동
+		// 저장하고 출력하는 것부터 실행하기
+		for (int i = 0; i < 8; i++)
+		{
+			if (i % 2 == 0)
+			{	//8방향 중 상우하좌 방향(0,2,4,6인덱스)
+				//공통 적용 요소
+				divRect[div_rect_count][i].r = rt[select_rect].r;
+				divRect[div_rect_count][i].g = rt[select_rect].g;
+				divRect[div_rect_count][i].b = rt[select_rect].b;
+				divRect[div_rect_count][i].width = rt[select_rect].width / 2;
+				divRect[div_rect_count][i].height = rt[select_rect].height / 2;
+				//방향에 따라 달라지는 요소들
+				if (i == 0)	
+				{	//상
+					divRect[div_rect_count][i].midX = rt[select_rect].midX;
+					divRect[div_rect_count][i].midY = rt[select_rect].midY + rt[select_rect].height;
+					divRect[div_rect_count][i].dx = 0;
+					divRect[div_rect_count][i].dy = 1;
+				}
+				else if (i == 2)
+				{	//우
+					divRect[div_rect_count][i].midX = rt[select_rect].midX + rt[select_rect].width;
+					divRect[div_rect_count][i].midY = rt[select_rect].midY;
+					divRect[div_rect_count][i].dx = 1;
+					divRect[div_rect_count][i].dy = 0;
+				}	
+				else if (i == 4)
+				{	//하
+					divRect[div_rect_count][i].midX = rt[select_rect].midX;
+					divRect[div_rect_count][i].midY = rt[select_rect].midY - rt[select_rect].height;
+					divRect[div_rect_count][i].dx = 0;
+					divRect[div_rect_count][i].dy = -1;
+				}
+				else
+				{	//좌
+					divRect[div_rect_count][i].midX = rt[select_rect].midX - rt[select_rect].width;
+					divRect[div_rect_count][i].midY = rt[select_rect].midY;
+					divRect[div_rect_count][i].dx = -1;
+					divRect[div_rect_count][i].dy = 0;
+				}
+			}
+			else
+			{	//8방향 중 대각선 부분은 없는 셈이다.
+				divRect[div_rect_count][i].midX = 0;
+				divRect[div_rect_count][i].midY = 0;
+				divRect[div_rect_count][i].r = 0;
+				divRect[div_rect_count][i].g = 0;
+				divRect[div_rect_count][i].b = 0;
+				divRect[div_rect_count][i].width = 0;
+				divRect[div_rect_count][i].height = 0;
+				divRect[div_rect_count][i].dx = 0;
+				divRect[div_rect_count][i].dy = 0;
+			}
+		}
+		div_rect_count++;
+		// 2. 4, 대각선 이동
+		// 3. 4, 쪼개지고, 한쪽 방향으로 같이 이동
+		// 4. 8, 8방향 이동
 	}
 }
 
@@ -122,7 +191,17 @@ void draw()
 	{
 		glColor3f(rt[i].r, rt[i].g, rt[i].b);
 		glRectf(rt[i].midX - (rt[i].width / 2), rt[i].midY - (rt[i].height / 2),
-			rt[i].midX + (rt[i].width / 2), rt[i].midY + (rt[i].height / 2));
+				rt[i].midX + (rt[i].width / 2), rt[i].midY + (rt[i].height / 2));
+	}
+	//draw DivRects
+	for (int i = 0; i < div_rect_count; i++)
+	{
+		for (int j = 0; j < 8; j++)
+		{
+			glColor3f(divRect[i][j].r, divRect[i][j].g, divRect[i][j].b);
+			glRectf(divRect[i][j].midX - (divRect[i][j].width / 2), divRect[i][j].midY - (divRect[i][j].height / 2),
+					divRect[i][j].midX + (divRect[i][j].width / 2), divRect[i][j].midY + (divRect[i][j].height / 2));
+		}
 	}
 }
 
@@ -200,7 +279,7 @@ void Mouse(int button, int state, int x, int y)
 	float mY = Win_to_GL_Y(y);
 	if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN)
 	{
-		devideRect(mX, mY);
+		divideRect(mX, mY);
 	}
 	glutPostRedisplay(); // refresh
 }
