158cdb9fc515621cd35594501d4674125e7b2374
sungwookcho@LAPTOP <fdsa7563@gmail.com>
2024-10-01 22:18:00 +0900

    Example2 시작 : 세이더 사용하기

---
+++ CG_00/vertex.glsl
@@ -0,0 +1,18 @@
+// vertex shader : 그리기 명령어에 의해 vertex array object로부터 버텍스 속성을 설정
+// 버텍스 변환, 텍스처 좌표 설정
+
+#version 330 core				// 사용 버전 선언
+					
+//in type in_variable_name;		// 필요한 변수 선언 1
+//out type out_variable_name;	// 필요한 변수 선언 2
+//in vec3 vPos;					// (예시)
+
+void main()						// 메인함수 인자값 없음
+{
+	// 입력 값을 처리
+
+	// 필요한 그래픽 작업 수행
+
+	// 출력 변수 저장
+	gl_Position = vec4(0.5, 0.0, 0.0, 1.0);
+}
