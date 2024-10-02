7bad9200a0f2db7e505bbaa12dc71e84af82c268
sungwookcho@LAPTOP <fdsa7563@gmail.com>
2024-10-02 10:46:09 +0900

    CG_01 추가 : GLSL 본격 시작

---
+++ CG_01/example4.cpp
@@ -0,0 +1,181 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <gl/glew.h>			//--- 필요한 헤더파일 include
+#include <gl/freeglut.h>
+#include <gl/freeglut_ext.h>
+
+#include <gl/glm/glm.hpp>
+#include <gl/glm/ext.hpp>
+#include <gl/glm/gtc/matrix_transform.hpp>
+
+//사용자 정의 함수
+GLvoid drawScene(GLvoid);
+GLvoid Reshape(int w, int h);
+GLvoid Keyboard(unsigned char key, int x, int y);
+char* filetobuf(const char* file);
+
+void make_vertexShaders();
+void make_fragmentShaders();
+GLuint make_shaderProgram();
+
+//필요 변수 선언
+GLint width, height;
+
+GLuint shaderProgramID;		//--- 세이더 프로그램 이름
+GLuint vertexShader;		//--- 버텍스 세이더 객체
+GLuint fragmentShader;		//--- 프래그먼트 세이더 객체
+
+GLint result;
+GLchar errorLog[512];
+
+void main(int argc, char** argv) //--- 윈도우 출력하고 콜백함수 설정
+{
+	width = 500;
+	height = 500;
+
+	//--- 윈도우 생성하기
+	glutInit(&argc, argv);									//--- glut 초기화
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);			//--- 디스플레이 모드 설정
+	glutInitWindowPosition(100, 100);						//--- 윈도우의 위치 지정
+	glutInitWindowSize(width, height);						//--- 윈도우의 크기 지정
+	glutCreateWindow("example4");							//--- 윈도우 생성(윈도우 이름)
+
+	//--- GLEW 초기화하기
+	glewExperimental = GL_TRUE;
+	if (glewInit() != GLEW_OK) //--- glew 초기화
+	{
+		std::cerr << "Unable to initialize GLEW" << std::endl;
+		exit(EXIT_FAILURE);
+	}
+	else
+		std::cout << "GLEW Initialized\n";
+
+	//--- 세이더 읽어와서 세이더 프로그램 만들기
+	make_vertexShaders();						//--- 버텍스 세이더 만들기
+	make_fragmentShaders();						//--- 프래그먼트 세이더 만들기
+	shaderProgramID = make_shaderProgram();		//--- 세이더 프로그램 만들기
+
+	glutDisplayFunc(drawScene);					//--- 출력 콜백함수의 지정
+	glutReshapeFunc(Reshape);					//--- 다시 그리기 콜백함수 지정
+	glutKeyboardFunc(Keyboard);	
+	glutMainLoop();								//--- 이벤트 처리 시작
+}
+GLvoid drawScene() //--- 콜백 함수: 그리기 콜백 함수
+{
+	GLfloat rColor, gColor, bColor;
+	rColor = 1.0;
+	gColor = 1.0;
+	bColor = 1.0;
+	glClearColor(rColor, gColor, bColor, 1.0f);			//--- 바탕색을 변경
+	glClear(GL_COLOR_BUFFER_BIT);						//--- 설정된 색으로 전체를 칠하기
+
+	glUseProgram(shaderProgramID);
+	//glPointSize(5.0);
+	glDrawArrays(GL_TRIANGLES, 0, 3);					//--- 삼각형 그리기: 0번 인덱스부터 3개의 버텍스를 사용하여 삼각형 그리기
+
+	glutSwapBuffers(); // 화면에 출력하기
+}
+GLvoid Reshape(int w, int h) //--- 콜백 함수: 다시 그리기 콜백 함수
+{
+	glViewport(0, 0, w, h);
+}
+GLvoid Keyboard(unsigned char key, int x, int y)
+{
+	switch (key)
+	{
+	case 'q':		// 프로그램 종료
+	{
+		std::cout << "--Quit--\n";
+		glutLeaveMainLoop(); // OpenGL 메인 루프 종료
+		break;
+	}
+	}
+	glutPostRedisplay(); //--- refresh
+}
+char* filetobuf(const char* file)
+{
+	FILE* fptr;
+	long length;
+	char* buf;
+	fptr = fopen(file, "rb");			// Open file for reading 
+	if (!fptr)							// Return NULL on failure 
+		return NULL;
+	fseek(fptr, 0, SEEK_END);			// Seek to the end of the file 
+	length = ftell(fptr);				// Find out how many bytes into the file we are 
+	buf = (char*)malloc(length + 1);	// Allocate a buffer for the entire length of the file and a null terminator 
+	fseek(fptr, 0, SEEK_SET);			// Go back to the beginning of the file 
+	fread(buf, length, 1, fptr);		// Read the contents of the file in to the buffer 
+	fclose(fptr);						// Close the file 
+	buf[length] = 0;					// Null terminator 
+	return buf;							// Return the buffer 
+}
+void make_vertexShaders()
+{
+	GLchar* vertexSource;
+
+	//--- 버텍스 세이더 읽어 저장하고 컴파일 하기
+	//--- filetobuf: 사용자정의 함수로 텍스트를 읽어서 문자열에 저장하는 함수
+
+	vertexSource = filetobuf("ex4_vertex.glsl");
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexSource, NULL);
+	glCompileShader(vertexShader);
+
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &result);
+	if (!result)
+	{
+		glGetShaderInfoLog(vertexShader, 512, NULL, errorLog);
+		std::cerr << "ERROR: vertex shader 컴파일 실패\n" << errorLog << std::endl;
+		return;
+	}
+}
+
+void make_fragmentShaders()
+{
+	GLchar* fragmentSource;
+
+	//--- 프래그먼트 세이더 읽어 저장하고 컴파일하기
+	fragmentSource = filetobuf("ex4_fragment.glsl"); // 프래그세이더 읽어오기
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentSource, NULL);
+	glCompileShader(fragmentShader);
+
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &result);
+	if (!result)
+	{
+		glGetShaderInfoLog(fragmentShader, 512, NULL, errorLog);
+		std::cerr << "ERROR: frag_shader 컴파일 실패\n" << errorLog << std::endl;
+		return;
+	}
+}
+
+GLuint make_shaderProgram()
+{
+	GLuint shaderID;
+	shaderID = glCreateProgram();						//--- 세이더 프로그램 만들기
+
+	glAttachShader(shaderID, vertexShader);				//--- 세이더 프로그램에 버텍스 세이더 붙이기
+	glAttachShader(shaderID, fragmentShader);			//--- 세이더 프로그램에 프래그먼트 세이더 붙이기
+
+	glLinkProgram(shaderID);							//--- 세이더 프로그램 링크하기
+
+	glDeleteShader(vertexShader);						//--- 세이더 객체를 세이더 프로그램에 링크했음으로, 세이더 객체 자체는 삭제 가능
+	glDeleteShader(fragmentShader);
+
+	glGetProgramiv(shaderID, GL_LINK_STATUS, &result);	// ---세이더가 잘 연결되었는지 체크하기
+	if (!result)
+	{
+		glGetProgramInfoLog(shaderID, 512, NULL, errorLog);
+		std::cerr << "ERROR: shader program 연결 실패\n" << errorLog << std::endl;
+		return false;
+	}
+
+	glUseProgram(shaderID); //--- 만들어진 세이더 프로그램 사용하기
+	//--- 여러 개의 세이더프로그램 만들 수 있고, 그 중 한개의 프로그램을 사용하려면
+	//--- glUseProgram 함수를 호출하여 사용 할 특정 프로그램을 지정한다.
+	//--- 사용하기 직전에 호출할 수 있다.
+	return shaderID;
+}
