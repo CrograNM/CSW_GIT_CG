472d0764270a212e69482764b799dabd438594c7
sungwookcho@LAPTOP <fdsa7563@gmail.com>
2024-10-07 13:23:38 +0900

    == 실습11 끝 ==

--- CG_01/1006_test11.cpp
+++ CG_01/1006_test11.cpp
@@ -48,6 +48,8 @@ float Win_to_GL_Y(int y)
 // 도형 관련 함수들
 void initFigure(); // 모든 사분면에 선, 삼각형, 사각형, 오각형 세팅
 void initQuardrant(int quardrant);
+void initMidFigure(int type);
+
 // 사분면 그리기
 GLfloat divLine[4][3] = {
 	{-1.0f, 0.0f, 0.0f},
@@ -75,11 +77,6 @@ FIGURE fg[5];		//fg[사분면], 1~4사분면, 0:중앙
 GLfloat	figure[MAX_FIGURE][TRI_COUNT * 3][3];	// 중앙값을 이용해, 삼각형 5개를 쓰자
 GLfloat	colorData[MAX_FIGURE][TRI_COUNT * 3][3];
 
-// 아래 변수들이 필요한지는 나중에 체크
-int figureCount = 0;						
-int figureType = 1;						
-int typeArray[MAX_FIGURE] = { 0, };		
-
 // 필요 변수 선언
 GLint width, height;
 GLchar* vertexSource, * fragmentSource;			//--- 소스코드 저장 변수
@@ -102,7 +99,7 @@ GLvoid InitBuffer();
 int timer_1 = false;
 float timerCount = 0.0f;
 #define FULLTIME 100
-#define TIMER_VELOCITY 50
+#define TIMER_VELOCITY 16
 void TimerFunction1(int value);
 
 void main(int argc, char** argv) //--- 윈도우 출력하고 콜백함수 설정
@@ -147,19 +144,20 @@ GLvoid drawScene()
 	glUseProgram(shaderProgramID);
 
 	// 가로, 세로 이등분 선 그리기
+	glLineWidth(1);
 	glBindVertexArray(vao[1]);
 	glDrawArrays(GL_LINES, 0, 4);
 
 	//--- 사용할 VAO 불러오기 (VAO에 VBO의 값들이 모두 저장되어 있는 것)
 	glBindVertexArray(vao[0]);
 
-	glLineWidth(2);
-	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+	/*glLineWidth(2);
+	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);*/
 	for (int i = 0; i < MAX_FIGURE; i++)
 	{
 		if (fg[i].exist == true)  // 도형이 존재하는 경우에만 그리기
 		{
-			/*if (fg[i].type == 2)
+			if (fg[i].type == 2)
 			{
 				glLineWidth(2);
 				glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
@@ -167,7 +165,11 @@ GLvoid drawScene()
 			else
 			{
 				glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-			}*/
+			}
+			if (timer_1 == true)
+			{
+				glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+			}
 			// 각 도형의 인덱스를 기반으로 삼각형 그리기
 			glDrawArrays(GL_TRIANGLES, i*15, 15);  // 총 5개의 삼각형을 그리기 위해 15개의 인덱스 사용
 		}
@@ -206,6 +208,85 @@ GLvoid Keyboard(unsigned char key, int x, int y)
 		}
 		break;
 	}
+	case 'l':	//line
+		if (timer_1 == false)
+		{
+			fg[0].exist = true;
+			fg[1].exist = false;
+			fg[2].exist = false;
+			fg[3].exist = false;
+			fg[4].exist = false;
+			initMidFigure(2);
+			
+			timerCount = 0;
+			std::cout << "line -> tri\n";
+			std::cout << "timer_1 ON\n";
+			timer_1 = true;
+			glutTimerFunc(TIMER_VELOCITY, TimerFunction1, 1);
+		}
+		break;
+	case 't':	//tri
+		if (timer_1 == false)
+		{
+			fg[0].exist = true;
+			fg[1].exist = false;
+			fg[2].exist = false;
+			fg[3].exist = false;
+			fg[4].exist = false;
+			initMidFigure(3);
+
+			timerCount = 0;
+			std::cout << "tri -> rect\n";
+			std::cout << "timer_1 ON\n";
+			timer_1 = true;
+			glutTimerFunc(TIMER_VELOCITY, TimerFunction1, 1);
+		}
+		break;
+	case 'r':	//rect
+		if (timer_1 == false)
+		{
+			fg[0].exist = true;
+			fg[1].exist = false;
+			fg[2].exist = false;
+			fg[3].exist = false;
+			fg[4].exist = false;
+			initMidFigure(4);
+
+			timerCount = 0;
+			std::cout << "rect -> penta\n";
+			std::cout << "timer_1 ON\n";
+			timer_1 = true;
+			glutTimerFunc(TIMER_VELOCITY, TimerFunction1, 1);
+		}
+		break;
+	case 'p':	//penta
+		if (timer_1 == false)
+		{
+			fg[0].exist = true;
+			fg[1].exist = false;
+			fg[2].exist = false;
+			fg[3].exist = false;
+			fg[4].exist = false;
+			initMidFigure(5);
+
+			timerCount = 0;
+			std::cout << "penta -> line\n";
+			std::cout << "timer_1 ON\n";
+			timer_1 = true;
+			glutTimerFunc(TIMER_VELOCITY, TimerFunction1, 1);
+		}
+		break;
+	case 'a':	//all
+		if (timer_1 == false)
+		{
+			fg[0].exist = false;
+			fg[1].exist = true;
+			fg[2].exist = true;
+			fg[3].exist = true;
+			fg[4].exist = true;
+			std::cout << "show ALL\n";
+		}
+		break;
 	}
 	glutPostRedisplay(); //--- refresh
 }
@@ -331,7 +412,8 @@ void initFigure()
 				colorData[i][j][k] = 0.0f;
 			}
 		}
-		typeArray[i] = 0;
+		fg[i].type = 2;
+		fg[i].exist = false;
 	}
 	// VBO에 새로운 삼각형 좌표 및 색상 데이터 추가
 	glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
@@ -351,7 +433,14 @@ void initQuardrant(int quardrant)
 	int x = 0;	//가독성을 위한 x
 	int y = 1;	//가독성을 위한 y
 
-	fg[quardrant].exist = true;
+	if (quardrant != 0)
+	{
+		fg[quardrant].exist = true;
+	}
+	else
+	{
+		fg[quardrant].exist = false;
+	}
 
 	float X[6];
 	float Y[6];
@@ -527,7 +616,7 @@ void initQuardrant(int quardrant)
 		figure[quardrant][14][x] = X[5];
 		figure[quardrant][14][y] = Y[5];
 	}
-	/*
+	
 	float random1 = generateRandomFloat(0.0f, 1.0f); //0~1의 값을 고정시킴
 	float random2 = generateRandomFloat(0.0f, 1.0f); //0~1의 값을 고정시킴
 	float random3 = generateRandomFloat(0.0f, 1.0f); //0~1의 값을 고정시킴
@@ -536,7 +625,7 @@ void initQuardrant(int quardrant)
 		colorData[quardrant][i][0] = random1; // R
 		colorData[quardrant][i][1] = random2; // G
 		colorData[quardrant][i][2] = random3; // B
-	}*/
+	}
 
 	glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
 	glBufferSubData(GL_ARRAY_BUFFER, quardrant * (TRI_COUNT * 9) * sizeof(GLfloat), (TRI_COUNT * 9) * sizeof(GLfloat), figure[quardrant]);
@@ -544,7 +633,166 @@ void initQuardrant(int quardrant)
 	glBindBuffer(GL_ARRAY_BUFFER, vbo[1]);
 	glBufferSubData(GL_ARRAY_BUFFER, quardrant * (TRI_COUNT * 9) * sizeof(GLfloat), (TRI_COUNT * 9) * sizeof(GLfloat), colorData[quardrant]);
 }
+void initMidFigure(int type)
+{
+	int x = 0;	//가독성을 위한 x
+	int y = 1;	//가독성을 위한 y
 
+	float X[6];
+	float Y[6];
+	switch (type)
+	{
+	case 2:
+	{	//선
+		fg[0].type = 2;
+		//0번 정점(중심점)
+		X[0] = mX[0];
+		Y[0] = mY[0];
+		//1번 정점
+		X[1] = mX[0] - FIGURE_SIZE;
+		Y[1] = mY[0] - FIGURE_SIZE;
+		//2번 정점
+		X[2] = mX[0] + FIGURE_SIZE;
+		Y[2] = mY[0] + FIGURE_SIZE;
+		//3번 정점
+		X[3] = mX[0];
+		Y[3] = mY[0];
+		//4번 정점
+		X[4] = mX[0];
+		Y[4] = mY[0];
+		//5번 정점
+		X[5] = mX[0];
+		Y[5] = mY[0];
+		break;
+	}
+	case 3:
+	{	//1사분면 : 삼각형
+		fg[0].type = 3;
+		//0번 
+		X[0] = mX[0];
+		Y[0] = mY[0];
+		//1번 
+		X[1] = mX[0] - FIGURE_SIZE;
+		Y[1] = mY[0] - FIGURE_SIZE;
+		//2번 
+		X[2] = mX[0] + FIGURE_SIZE;
+		Y[2] = mY[0] - FIGURE_SIZE;
+		//3번 
+		X[3] = mX[0];
+		Y[3] = mY[0] + FIGURE_SIZE;
+		//4번 
+		X[4] = mX[0];
+		Y[4] = mY[0] + FIGURE_SIZE;
+		//5번 
+		X[5] = mX[0];
+		Y[5] = mY[0] + FIGURE_SIZE;
+		break;
+	}
+	case 4:
+	{	//사각형
+		fg[0].type = 4;
+		//0번 
+		X[0] = mX[0];
+		Y[0] = mY[0];
+		//1번 
+		X[1] = mX[0] - FIGURE_SIZE;
+		Y[1] = mY[0] - FIGURE_SIZE;
+		//2번 
+		X[2] = mX[0] + FIGURE_SIZE;
+		Y[2] = mY[0] - FIGURE_SIZE;
+		//3번 
+		X[3] = mX[0];
+		Y[3] = mY[0] + FIGURE_SIZE;
+		//4번 
+		X[4] = mX[0] - FIGURE_SIZE;
+		Y[4] = mY[0] + FIGURE_SIZE;
+		//5번 
+		X[5] = mX[0] + FIGURE_SIZE;
+		Y[5] = mY[0] + FIGURE_SIZE;
+		break;
+	}
+	case 5:
+	{	//오각형
+		fg[0].type = 5;
+		//0번 
+		X[0] = mX[0];
+		Y[0] = mY[0];
+		//1번 
+		X[1] = mX[0] - FIGURE_SIZE;
+		Y[1] = mY[0] - FIGURE_SIZE;
+		//2번 
+		X[2] = mX[0] + FIGURE_SIZE;
+		Y[2] = mY[0] - FIGURE_SIZE;
+		//3번 
+		X[3] = mX[0];
+		Y[3] = mY[0] + FIGURE_SIZE + 0.05f;;
+		//4번 
+		X[4] = mX[0] - FIGURE_SIZE - 0.03f;
+		Y[4] = mY[0] + FIGURE_SIZE - 0.05f;
+		//5번 
+		X[5] = mX[0] + FIGURE_SIZE + 0.03f;
+		Y[5] = mY[0] + FIGURE_SIZE - 0.05f;
+		break;
+	}
+	}
+
+	//정점 15개에 각 인덱스를 대입한다. (삼각형 5개 * 정점 3개 = 정점 15개)
+	if (true)
+	{
+		// 012인덱스
+		figure[0][0][x] = X[0];
+		figure[0][0][y] = Y[0];
+
+		figure[0][1][x] = X[1];
+		figure[0][1][y] = Y[1];
+
+		figure[0][2][x] = X[2];
+		figure[0][2][y] = Y[2];
+
+		// 014인덱스
+		figure[0][3][x] = X[0];
+		figure[0][3][y] = Y[0];
+
+		figure[0][4][x] = X[1];
+		figure[0][4][y] = Y[1];
+
+		figure[0][5][x] = X[4];
+		figure[0][5][y] = Y[4];
+
+		// 025인덱스
+		figure[0][6][x] = X[0];
+		figure[0][6][y] = Y[0];
+
+		figure[0][7][x] = X[2];
+		figure[0][7][y] = Y[2];
+
+		figure[0][8][x] = X[5];
+		figure[0][8][y] = Y[5];
+
+		// 034인덱스
+		figure[0][9][x] = X[0];
+		figure[0][9][y] = Y[0];
+
+		figure[0][10][x] = X[3];
+		figure[0][10][y] = Y[3];
+
+		figure[0][11][x] = X[4];
+		figure[0][11][y] = Y[4];
+
+		// 035인덱스
+		figure[0][12][x] = X[0];
+		figure[0][12][y] = Y[0];
+
+		figure[0][13][x] = X[3];
+		figure[0][13][y] = Y[3];
+
+		figure[0][14][x] = X[5];
+		figure[0][14][y] = Y[5];
+	}
+
+	glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
+	glBufferSubData(GL_ARRAY_BUFFER, 0 * (TRI_COUNT * 9) * sizeof(GLfloat), (TRI_COUNT * 9) * sizeof(GLfloat), figure[0]);
+}
 //타이머
 //int timerCount = 0;
 //#define FULLTIME 100
@@ -565,96 +813,105 @@ void TimerFunction1(int value)
 			timerCount += 1.0f;
 			for (int i = 0; i < MAX_FIGURE; i++)
 			{
-				switch (fg[i].type)
+				if (fg[i].exist == true)
 				{
-				case 2: // 선 -> 삼각형
-				{
-					// 2,3,4,5 정점 이동
-					// -> 정점 = (출발점) + ((도착) - (출발)) x (timerCount / FULLTIME)
-					// 정점 2 : 2,7인덱스 -> 삼각형의 오른쪽 아래로 이동 (y만 변한다)
-					figure[i][2][y] = (mY[i] + FIGURE_SIZE) + ((mY[i] - FIGURE_SIZE) - (mY[i] + FIGURE_SIZE)) * (timerCount / FULLTIME);
-					figure[i][7][y] = (mY[i] + FIGURE_SIZE) + ((mY[i] - FIGURE_SIZE) - (mY[i] + FIGURE_SIZE)) * (timerCount / FULLTIME);
-					// 정점 3,4,5 : 10,13, 5,11, 8,14인덱스 -> 삼각형의 위로 이동 (y만 변한다)
-					figure[i][10][y] = (mY[i]) + ((mY[i] + FIGURE_SIZE) - (mY[i])) * (timerCount / FULLTIME);
-					figure[i][13][y] = (mY[i]) + ((mY[i] + FIGURE_SIZE) - (mY[i])) * (timerCount / FULLTIME);
+					switch (fg[i].type)
+					{
+					case 2: // 선 -> 삼각형
+					{
+						// 2,3,4,5 정점 이동
+						// -> 정점 = (출발점) + ((도착) - (출발)) x (timerCount / FULLTIME)
+						// 정점 2 : 2,7인덱스 -> 삼각형의 오른쪽 아래로 이동 (y만 변한다)
+						figure[i][2][y] = (mY[i] + FIGURE_SIZE) + ((mY[i] - FIGURE_SIZE) - (mY[i] + FIGURE_SIZE)) * (timerCount / FULLTIME);
+						figure[i][7][y] = (mY[i] + FIGURE_SIZE) + ((mY[i] - FIGURE_SIZE) - (mY[i] + FIGURE_SIZE)) * (timerCount / FULLTIME);
+						// 정점 3,4,5 : 10,13, 5,11, 8,14인덱스 -> 삼각형의 위로 이동 (y만 변한다)
+						figure[i][10][y] = (mY[i]) + ((mY[i] + FIGURE_SIZE) - (mY[i])) * (timerCount / FULLTIME);
+						figure[i][13][y] = (mY[i]) + ((mY[i] + FIGURE_SIZE) - (mY[i])) * (timerCount / FULLTIME);
 
-					figure[i][5][y] = (mY[i]) + ((mY[i] + FIGURE_SIZE) - (mY[i])) * (timerCount / FULLTIME);
-					figure[i][11][y] =(mY[i]) + ((mY[i] + FIGURE_SIZE) - (mY[i])) * (timerCount / FULLTIME);
+						figure[i][5][y] = (mY[i]) + ((mY[i] + FIGURE_SIZE) - (mY[i])) * (timerCount / FULLTIME);
+						figure[i][11][y] = (mY[i]) + ((mY[i] + FIGURE_SIZE) - (mY[i])) * (timerCount / FULLTIME);
 
-					figure[i][8][y] = (mY[i]) + ((mY[i] + FIGURE_SIZE) - (mY[i])) * (timerCount / FULLTIME);
-					figure[i][14][y] =(mY[i]) + ((mY[i] + FIGURE_SIZE) - (mY[i])) * (timerCount / FULLTIME);
-					break;
-				}
-				case 3: // 삼각형 -> 사각형
-				{
-					// 4,5 정점 이동 (x만 변한다)
-					// 정점 4 : 5,11 -> 사각형의 왼쪽으로 이동 
-					figure[i][5][x] = mX[i] + ((mX[i] - FIGURE_SIZE) - mX[i]) * (timerCount / FULLTIME);
-					figure[i][11][x] = mX[i] + ((mX[i] - FIGURE_SIZE) - mX[i]) * (timerCount / FULLTIME);
-					// 정점 5 : 8,14 -> 사각형의 오른쪽으로 이동
-					figure[i][8][x] = mX[i] + ((mX[i] + FIGURE_SIZE) - mX[i]) * (timerCount / FULLTIME);
-					figure[i][14][x] = mX[i] + ((mX[i] + FIGURE_SIZE) - mX[i]) * (timerCount / FULLTIME);
-					break;
-				}
-				case 4: // 사각형 -> 오각형
-				{
-					// 정점 3,4,5 : 10,13, 5,11, 8,14인덱스 -> 삼각형의 위로 이동 (x y 모두 변함, 정점 3은 y만 변함)
-					// // -> 정점 = (출발점) + ((도착) - (출발)) x (timerCount / FULLTIME)
-					//figure[i][10][x] = mX[i] + ((mY[i] + FIGURE_SIZE) - mX[i]) * (timerCount / FULLTIME);
-					//figure[i][13][x] = mX[i] + ((mY[i] + FIGURE_SIZE) - mX[i]) * (timerCount / FULLTIME);
-					figure[i][5][x] = (mX[i] - FIGURE_SIZE) + ((mX[i] - FIGURE_SIZE - 0.03f) - (mX[i] - FIGURE_SIZE)) * (timerCount / FULLTIME);
-					figure[i][11][x] = (mX[i] - FIGURE_SIZE) + ((mX[i] - FIGURE_SIZE - 0.03f) - (mX[i] - FIGURE_SIZE)) * (timerCount / FULLTIME);
+						figure[i][8][y] = (mY[i]) + ((mY[i] + FIGURE_SIZE) - (mY[i])) * (timerCount / FULLTIME);
+						figure[i][14][y] = (mY[i]) + ((mY[i] + FIGURE_SIZE) - (mY[i])) * (timerCount / FULLTIME);
+						break;
+					}
+					case 3: // 삼각형 -> 사각형
+					{
+						// 4,5 정점 이동 (x만 변한다)
+						// 정점 4 : 5,11 -> 사각형의 왼쪽으로 이동 
+						figure[i][5][x] = mX[i] + ((mX[i] - FIGURE_SIZE) - mX[i]) * (timerCount / FULLTIME);
+						figure[i][11][x] = mX[i] + ((mX[i] - FIGURE_SIZE) - mX[i]) * (timerCount / FULLTIME);
+						// 정점 5 : 8,14 -> 사각형의 오른쪽으로 이동
+						figure[i][8][x] = mX[i] + ((mX[i] + FIGURE_SIZE) - mX[i]) * (timerCount / FULLTIME);
+						figure[i][14][x] = mX[i] + ((mX[i] + FIGURE_SIZE) - mX[i]) * (timerCount / FULLTIME);
+						break;
+					}
+					case 4: // 사각형 -> 오각형
+					{
+						// 정점 3,4,5 : 10,13, 5,11, 8,14인덱스 -> 삼각형의 위로 이동 (x y 모두 변함, 정점 3은 y만 변함)
+						// // -> 정점 = (출발점) + ((도착) - (출발)) x (timerCount / FULLTIME)
+						//figure[i][10][x] = mX[i] + ((mY[i] + FIGURE_SIZE) - mX[i]) * (timerCount / FULLTIME);
+						//figure[i][13][x] = mX[i] + ((mY[i] + FIGURE_SIZE) - mX[i]) * (timerCount / FULLTIME);
+						figure[i][5][x] = (mX[i] - FIGURE_SIZE) + ((mX[i] - FIGURE_SIZE - 0.03f) - (mX[i] - FIGURE_SIZE)) * (timerCount / FULLTIME);
+						figure[i][11][x] = (mX[i] - FIGURE_SIZE) + ((mX[i] - FIGURE_SIZE - 0.03f) - (mX[i] - FIGURE_SIZE)) * (timerCount / FULLTIME);
 
-					figure[i][8][x] = (mX[i] + FIGURE_SIZE) + ((mX[i] + FIGURE_SIZE + 0.03f) - (mX[i] + FIGURE_SIZE)) * (timerCount / FULLTIME);
-					figure[i][14][x] = (mX[i] + FIGURE_SIZE) + ((mX[i] + FIGURE_SIZE + 0.03f) - (mX[i] + FIGURE_SIZE)) * (timerCount / FULLTIME);
+						figure[i][8][x] = (mX[i] + FIGURE_SIZE) + ((mX[i] + FIGURE_SIZE + 0.03f) - (mX[i] + FIGURE_SIZE)) * (timerCount / FULLTIME);
+						figure[i][14][x] = (mX[i] + FIGURE_SIZE) + ((mX[i] + FIGURE_SIZE + 0.03f) - (mX[i] + FIGURE_SIZE)) * (timerCount / FULLTIME);
 
-					figure[i][10][y] = (mY[i] + FIGURE_SIZE) + ((mY[i] + FIGURE_SIZE + 0.05f) - (mY[i] + FIGURE_SIZE)) * (timerCount / FULLTIME);
-					figure[i][13][y] = (mY[i] + FIGURE_SIZE) + ((mY[i] + FIGURE_SIZE + 0.05f) - (mY[i] + FIGURE_SIZE)) * (timerCount / FULLTIME);
+						figure[i][10][y] = (mY[i] + FIGURE_SIZE) + ((mY[i] + FIGURE_SIZE + 0.05f) - (mY[i] + FIGURE_SIZE)) * (timerCount / FULLTIME);
+						figure[i][13][y] = (mY[i] + FIGURE_SIZE) + ((mY[i] + FIGURE_SIZE + 0.05f) - (mY[i] + FIGURE_SIZE)) * (timerCount / FULLTIME);
 
-					figure[i][5][y] = (mY[i] + FIGURE_SIZE) + ((mY[i] + FIGURE_SIZE - 0.05f) - (mY[i] + FIGURE_SIZE)) * (timerCount / FULLTIME);
-					figure[i][11][y] = (mY[i] + FIGURE_SIZE) + ((mY[i] + FIGURE_SIZE - 0.05f) - (mY[i] + FIGURE_SIZE)) * (timerCount / FULLTIME);
+						figure[i][5][y] = (mY[i] + FIGURE_SIZE) + ((mY[i] + FIGURE_SIZE - 0.05f) - (mY[i] + FIGURE_SIZE)) * (timerCount / FULLTIME);
+						figure[i][11][y] = (mY[i] + FIGURE_SIZE) + ((mY[i] + FIGURE_SIZE - 0.05f) - (mY[i] + FIGURE_SIZE)) * (timerCount / FULLTIME);
 
-					figure[i][8][y] = (mY[i] + FIGURE_SIZE) + ((mY[i] + FIGURE_SIZE - 0.05f) - (mY[i] + FIGURE_SIZE)) * (timerCount / FULLTIME);
-					figure[i][14][y] = (mY[i] + FIGURE_SIZE) + ((mY[i] + FIGURE_SIZE - 0.05f) - (mY[i] + FIGURE_SIZE)) * (timerCount / FULLTIME);
-					break;
-				}
-				case 5: // 오각형 -> 선
-				{
-					// 2,3,4,5 정점 이동
-					// -> 정점 = (출발점) + ((도착) - (출발)) x (timerCount / FULLTIME)
-					// 정점 2 : 2,7 -> 선 정점 2로 이동
-					figure[i][2][x] = (mX[i] + FIGURE_SIZE) + ((mX[i] + FIGURE_SIZE) - (mX[i] + FIGURE_SIZE)) * (timerCount / FULLTIME);
-					figure[i][7][x] = (mX[i] + FIGURE_SIZE) + ((mX[i] + FIGURE_SIZE) - (mX[i] + FIGURE_SIZE)) * (timerCount / FULLTIME);
-					figure[i][2][y] = (mY[i] - FIGURE_SIZE) + ((mY[i] + FIGURE_SIZE) - (mY[i] - FIGURE_SIZE)) * (timerCount / FULLTIME);
-					figure[i][7][y] = (mY[i] - FIGURE_SIZE) + ((mY[i] + FIGURE_SIZE) - (mY[i] - FIGURE_SIZE)) * (timerCount / FULLTIME);
-					// 정점 3 : 10,13 -> 선 정점 0로 이동
-					figure[i][10][x] = (mX[i]) + ((mX[i]) - (mX[i])) * (timerCount / FULLTIME);
-					figure[i][13][x] = (mX[i]) + ((mX[i]) - (mX[i])) * (timerCount / FULLTIME);
-					figure[i][10][y] = (mY[i] + FIGURE_SIZE + 0.05f) + ((mY[i]) - (mY[i] + FIGURE_SIZE + 0.05f)) * (timerCount / FULLTIME);
-					figure[i][13][y] = (mY[i] + FIGURE_SIZE + 0.05f) + ((mY[i]) - (mY[i] + FIGURE_SIZE + 0.05f)) * (timerCount / FULLTIME);
-					// 정점 4 : 5,11 -> 선 정점 0로 이동 
-					figure[i][5][x] = (mX[i] - FIGURE_SIZE - 0.03f) + ((mX[i]) - (mX[i] - FIGURE_SIZE - 0.03f)) * (timerCount / FULLTIME);
-					figure[i][11][x] = (mX[i] - FIGURE_SIZE - 0.03f) + ((mX[i]) - (mX[i] - FIGURE_SIZE - 0.03f)) * (timerCount / FULLTIME);
-					figure[i][5][y] = (mY[i] + FIGURE_SIZE - 0.05f) + ((mY[i]) - (mY[i] + FIGURE_SIZE - 0.05f)) * (timerCount / FULLTIME);
-					figure[i][11][y] = (mY[i] + FIGURE_SIZE - 0.05f) + ((mY[i]) - (mY[i] + FIGURE_SIZE - 0.05f)) * (timerCount / FULLTIME);
-					// 정점 5 : 8,14 -> 선 정점 0로 이동
-					figure[i][8][x] = (mX[i] + FIGURE_SIZE + 0.03f) + ((mX[i]) - (mX[i] + FIGURE_SIZE + 0.03f)) * (timerCount / FULLTIME);
-					figure[i][14][x] = (mX[i] + FIGURE_SIZE + 0.03f) + ((mX[i]) - (mX[i] + FIGURE_SIZE + 0.03f)) * (timerCount / FULLTIME);
-					figure[i][8][y] = (mY[i] + FIGURE_SIZE - 0.05f) + ((mY[i]) - (mY[i] + FIGURE_SIZE - 0.05f)) * (timerCount / FULLTIME);
-					figure[i][14][y] = (mY[i] + FIGURE_SIZE - 0.05f) + ((mY[i]) - (mY[i] + FIGURE_SIZE - 0.05f)) * (timerCount / FULLTIME);
-					break;
-				}
+						figure[i][8][y] = (mY[i] + FIGURE_SIZE) + ((mY[i] + FIGURE_SIZE - 0.05f) - (mY[i] + FIGURE_SIZE)) * (timerCount / FULLTIME);
+						figure[i][14][y] = (mY[i] + FIGURE_SIZE) + ((mY[i] + FIGURE_SIZE - 0.05f) - (mY[i] + FIGURE_SIZE)) * (timerCount / FULLTIME);
+						break;
+					}
+					case 5: // 오각형 -> 선
+					{
+						// 2,3,4,5 정점 이동
+						// -> 정점 = (출발점) + ((도착) - (출발)) x (timerCount / FULLTIME)
+						// 정점 2 : 2,7 -> 선 정점 2로 이동
+						figure[i][2][x] = (mX[i] + FIGURE_SIZE) + ((mX[i] + FIGURE_SIZE) - (mX[i] + FIGURE_SIZE)) * (timerCount / FULLTIME);
+						figure[i][7][x] = (mX[i] + FIGURE_SIZE) + ((mX[i] + FIGURE_SIZE) - (mX[i] + FIGURE_SIZE)) * (timerCount / FULLTIME);
+						figure[i][2][y] = (mY[i] - FIGURE_SIZE) + ((mY[i] + FIGURE_SIZE) - (mY[i] - FIGURE_SIZE)) * (timerCount / FULLTIME);
+						figure[i][7][y] = (mY[i] - FIGURE_SIZE) + ((mY[i] + FIGURE_SIZE) - (mY[i] - FIGURE_SIZE)) * (timerCount / FULLTIME);
+						// 정점 3 : 10,13 -> 선 정점 0로 이동
+						figure[i][10][x] = (mX[i]) + ((mX[i]) - (mX[i])) * (timerCount / FULLTIME);
+						figure[i][13][x] = (mX[i]) + ((mX[i]) - (mX[i])) * (timerCount / FULLTIME);
+						figure[i][10][y] = (mY[i] + FIGURE_SIZE + 0.05f) + ((mY[i]) - (mY[i] + FIGURE_SIZE + 0.05f)) * (timerCount / FULLTIME);
+						figure[i][13][y] = (mY[i] + FIGURE_SIZE + 0.05f) + ((mY[i]) - (mY[i] + FIGURE_SIZE + 0.05f)) * (timerCount / FULLTIME);
+						// 정점 4 : 5,11 -> 선 정점 0로 이동 
+						figure[i][5][x] = (mX[i] - FIGURE_SIZE - 0.03f) + ((mX[i]) - (mX[i] - FIGURE_SIZE - 0.03f)) * (timerCount / FULLTIME);
+						figure[i][11][x] = (mX[i] - FIGURE_SIZE - 0.03f) + ((mX[i]) - (mX[i] - FIGURE_SIZE - 0.03f)) * (timerCount / FULLTIME);
+						figure[i][5][y] = (mY[i] + FIGURE_SIZE - 0.05f) + ((mY[i]) - (mY[i] + FIGURE_SIZE - 0.05f)) * (timerCount / FULLTIME);
+						figure[i][11][y] = (mY[i] + FIGURE_SIZE - 0.05f) + ((mY[i]) - (mY[i] + FIGURE_SIZE - 0.05f)) * (timerCount / FULLTIME);
+						// 정점 5 : 8,14 -> 선 정점 0로 이동
+						figure[i][8][x] = (mX[i] + FIGURE_SIZE + 0.03f) + ((mX[i]) - (mX[i] + FIGURE_SIZE + 0.03f)) * (timerCount / FULLTIME);
+						figure[i][14][x] = (mX[i] + FIGURE_SIZE + 0.03f) + ((mX[i]) - (mX[i] + FIGURE_SIZE + 0.03f)) * (timerCount / FULLTIME);
+						figure[i][8][y] = (mY[i] + FIGURE_SIZE - 0.05f) + ((mY[i]) - (mY[i] + FIGURE_SIZE - 0.05f)) * (timerCount / FULLTIME);
+						figure[i][14][y] = (mY[i] + FIGURE_SIZE - 0.05f) + ((mY[i]) - (mY[i] + FIGURE_SIZE - 0.05f)) * (timerCount / FULLTIME);
+						break;
+					}
+					}
 				}
 			}
+			
+			//타이머 종료 확인
 			if (timerCount >= FULLTIME)
 			{
+				std::cout << "timer_1 OFF\n";
 				timer_1 = false;
 				for (int i = 0; i < MAX_FIGURE; i++)
 				{
-					fg[i].type++;
-					if (fg[i].type > 5)
+					if (fg[i].exist == true)
 					{
-						fg[i].type = 2;
+						fg[i].type++;
+						if (fg[i].type > 5)
+						{
+							fg[i].type = 2;
+						}
 					}
 				}
 			}
